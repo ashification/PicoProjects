988891deebfb13939720372afe6b869597b2d7cf
Aisling Lee <aislinglee@gmail.com>
2025-09-25 20:08:54 +0100

    Update humidity_and_temp.py to include pico and micropython version

--- Task_5-HumidityTemp/humidity_and_temp.py
+++ Task_5-HumidityTemp/humidity_and_temp.py
@@ -1,6 +1,7 @@
 # Code Source from https://www.instructables.com/DHT11-With-Raspberry-Pi-Pico/
 # Slight modifications to the code given in the link taken from thread here https://forums.raspberrypi.com/viewtopic.php?t=343485
-# Works with current version of pico (RPI2040) and micropython (raspberry Pi Pico) installed 2025-09-25
+# Works with current version of MicroPython v1.25.0 on 2025-04-15; Raspberry Pi Pico 2 W with RP2350
+
 
 import dht 
 import machine 
@@ -13,4 +14,5 @@ data_dht.measure()
 print() 
 print ( f" temperature (C) :{data_dht.temperature()}") 
 print ( f" humidity (%): {data_dht.humidity()} ")
+
 
