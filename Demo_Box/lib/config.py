4f6945464c8443190f0f4a46fb70d9c4557b2436
Aisling Lee <aislinglee@gmail.com>
2025-09-26 09:28:54 +0100

    Delete Demo_Box/lib/config.py

--- Demo_Box/lib/config.py
+++
@@ -1,6 +0,0 @@
-wifi_ssid = "insert wifi name here"
-wifi_password = 'insert wifi password here'
-mqtt_server = b'mqtt server ip here'
-mqtt_username = b'mqtt user name here'
-mqtt_password = b'mqtt password name here'
-mqtt_port = <mqtt port name here>
